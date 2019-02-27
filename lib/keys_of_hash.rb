8d2d4fa4f61ec0695243569f0f056c53f37b2053
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-27 21:34:42 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -5,7 +5,7 @@ class Hash
      self.each do |key, value|
   binding.pry
        if(value.to_s == args)
-          array << key.to-s
+          array << key.to_s
        end
      end
      array# code goes here
