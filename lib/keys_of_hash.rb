d7cb4408749327fd191380e0a4f7faf7915f30bb
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-27 21:41:54 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,11 +3,11 @@ class Hash
   def keys_of(*args)
     array =[]
      self.each do |key, value|
-  
+
        if(args.include?(value))
           array << key
        end
      end
-     array# code goes here
+     array
   end
 end
